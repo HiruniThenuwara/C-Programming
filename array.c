b9fdbdd68f87dabcdd940be639af9e89b99d738b
Hiruni Thenuwara <119315041+HiruniThenuwara@users.noreply.github.com>
2022-12-07 16:31:04 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main (){
+	int values[]={65,55,45,35,25};
+	int index;
+	int counter;
+	int tot=0;
+	float average;
+	for(index=0;index<5;index++){
+		printf("%d element :%d\n",index,values[index]);
+	}
+	for (counter=65;counter>=25;counter=counter-10){
+		tot=tot+counter;
+	}
+    printf("Total:%d\n",tot);
+	average=tot/index;
+	printf("Average:%f\n",average);
+	return 0;
+}
